a3a7d1c8860dfeeb1cf7b29583779ce9fe78b409
NoumanImran <101354576+NoumanImran@users.noreply.github.com>
2022-03-11 21:39:28 +0500

    Create Assignment2.dart

---
+++ Assignment2.dart
@@ -0,0 +1,29 @@
+void main() {
+  
+
+  String id = "15766313";
+  int phoneNum = int.parse(id);
+  print(phoneNum);
+  //   OUTPUT:15766313
+  
+  
+  String num = "2033.12244";
+  double newSallary = double.parse(num);
+  print(newSallary);
+  //   OUTPUT:2033.12244
+
+   String name = 'Nomi';
+   int books = 43432;
+   print('$name has ${books.toString()} books left');
+   //OUTPUT: Nomi has 43432 books left
+
+  
+    double toInt = 985.351;
+    print(toInt.toInt());
+    //OUTPUT : 985
+
+    int temperature = 874363;
+    print(temperature.toDouble());
+    //OUTPUT : 874363.0
+
+}
